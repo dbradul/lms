a3cb96d5f5998f68c603bc0c0d7198cb0a325407
Bradul Dmytro <dbradul@gmail.com>
2021-09-30 23:15:37 +0300

    Init commit

---
+++ students/models.py
@@ -0,0 +1,10 @@
+import datetime
+
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    first_name = models.CharField(max_length=80, null=False)
+    last_name = models.CharField(max_length=80, null=False)
+    email = models.EmailField(max_length=120, null=True)
+    birthdate = models.DateField(null=True, default=datetime.date.today)
