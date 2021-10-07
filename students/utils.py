f48a030f78972e78c50405bd27ba000ac9a52653
Bradul Dmytro <dbradul@gmail.com>
2021-10-07 10:52:23 +0300

    Work with Queryset

---
+++ students/utils.py
@@ -0,0 +1,7 @@
+
+
+def format_records(records):
+    if not records:
+        return '(Empty recordset)'
+    return '<br>'.join(str(rec) for rec in records)
+
--- students/views.py
+++ students/views.py
@@ -2,5 +2,46 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+from webargs import fields
+from webargs.djangoparser import use_kwargs, use_args
+
+from students.models import Student
+from students.utils import format_records
+from django.core.exceptions import BadRequest
+from webargs import djangoparser
+
+
 def hello(request):
     return HttpResponse('SUCCESS')
+
+
+parser = djangoparser.DjangoParser()
+
+
+@parser.error_handler
+def handle_error(error, req, schema, *, error_status_code, error_headers):
+    raise BadRequest(error.messages)
+
+
+@parser.use_args(
+    {
+        "first_name": fields.Str(
+            required=False,
+        ),
+        "text": fields.Str(
+            required=False,
+            validate=[lambda x: False]
+        ),
+    },
+    location="query",
+)
+def get_students(request, params):
+
+    students = Student.objects.all()
+
+    for param_name, param_value in params.items():
+        students = students.filter(**{param_name: param_value})
+
+    result = format_records(students)
+
+    return HttpResponse(result)
